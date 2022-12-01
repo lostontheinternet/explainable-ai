a443cf1d379833473e41437b99154201dabc065e
Keleihaulani <terai.kelei@gmail.com>
2022-12-01 15:29:04 +0100

    commented rmn-gradcam and img_functions

--- img_functions.py
+++ img_functions.py
@@ -1,17 +1,26 @@
 from deepface import DeepFace
+
 import random
+import os
+
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+
 import cv2
-
 from PIL import Image
-import os, os.path
+from torchvision import transforms
 
-# Load all images from the BV dataset
-def load_images() :
-    img_list = []
-    path = ".\datasets\BV"
+def load_ims(path) :
+
+    """
+    Load all images from a given path
+    Parameter : path of the directory from which to load all images
+
+    Output : array containing loaded images (in PIL type)
+    """
+
+    img_list = [] # loaded images are temporarily stored in a list
     for f in os.listdir(path):
         try:
             img = Image.open(os.path.join(path,f))
@@ -21,20 +30,56 @@ def load_images() :
             print(e)
     return np.array(img_list)
 
-# Display given number of RGB images from an array, starting from a given position in the array
-def disp_ims(imgs, nb_imgs=10, start_index=0) :
-    plt.figure(figsize=(10,10))
-    for i in range(nb_imgs) :
-        plt.subplot(3, 3, i + 1)
-        plt.imshow(imgs[start_index+i])
-        # plt.title(image)
-        plt.axis("off")
+def resize_ims(im_arr,targetsize) :
 
-# Display grayscale image
-def gr_show_im(gray_im) :
-    arr = np.array(gray_im)
-    plt.imshow(arr, cmap='gray', vmin=0, vmax=1)
-    plt.show()
+    """
+    Resize all images from a given numpy array
+    Parameters :
+        im_arr : array of images (type PIL)
+        sizeH, sizeW : dimensions to resize images to
+    
+    Output : array of resized images (type PIL)
+    """
+
+    H,W = targetsize
+    result=[] # resized images are temporarily stored in a list
+    resize=transforms.Resize(size=(H,W))
+    for img in im_arr:
+        img = resize(img)
+        result.append(img)
+    return np.array(result)
+
+def crop_ims(im_arr,originalsize,targetsize):
+
+    """
+    Crop images, each resulting image is centered on the original one
+    The initial intention was to use this function to remove the padding of the Eye-Tracking images
+    Parameters :
+        im_arr : array of images (PIL type)
+        originalsize : original size of images
+        targetsize : desired size of cropped images
+
+    Output : array of cropped images (PIL type)
+    """
+
+    (origH,origW) = originalsize
+    (targH,targW) = targetsize
+    img_list = [] # cropped images are temporarily stored in a list
+
+    for image in im_arr :
+        im = np.array(image)
+        # compute position of the desired window
+        min_y=(origH//2)-(targH//2)
+        max_y=(origH//2)+(targH//2)
+        min_x=(origW//2)-(targW//2)
+        max_x=(origW//2)+(targW//2)
+        # crop image
+        crop = im[min_y:max_y, min_x:max_x]
+        # convert to PIL Image type
+        pil_crop = Image.fromarray(crop.astype('uint8'), 'RGB')
+        img_list.append(pil_crop)
+
+    return np.array(img_list)
 
 def preproc_deepface (img) :
     target_size=(48,48)
@@ -42,8 +87,9 @@ def preproc_deepface (img) :
     enforce_detection = False
     detector_backend = 'opencv'
     return_region = False
-    result = DeepFace.functions.preprocess_face(img = img, target_size = target_size, grayscale = grayscale, enforce_detection = enforce_detection, detector_backend = detector_backend, return_region = return_region)
-
+    result = DeepFace.functions.preprocess_face(img = img, target_size = target_size,
+        grayscale = grayscale, enforce_detection = enforce_detection,
+        detector_backend = detector_backend, return_region = return_region)
     return result
 
 def preproc_fer (img_array) :
